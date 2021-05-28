7933f3d8e4caa425aed9f9364f3fe0057f6e20d7
Pavan <5058918-gitme3@users.noreply.gitlab.com>
2021-05-28 19:20:54 +0530

    new char

--- char.sh
+++ char.sh
@@ -2,7 +2,11 @@
 
 ## to get name of the script
 echo $0
-
+## first argument to be passed
 echo $1
-
+## second argument to be passed
 echo $2
+## all the arguments to be passed
+
+echo $*
+echo $@
