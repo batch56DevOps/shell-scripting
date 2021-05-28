a81539bc534d2f4be975f8c203dc0d38ce916baf
Pavan <5058918-gitme3@users.noreply.gitlab.com>
2021-05-28 17:20:41 +0530

    new file

--- sample.sh
+++ sample.sh
@@ -12,3 +12,7 @@ echo -e "\tthis helps to demonstrate and use
          escape sequence. The rules and restrictions vary from state to state,
          and it can get difficult to keep a track of it all.
          That's why we've come up with a one-stop solution for your lockdown queries"
+
+echo -e "\e[31Hello this is red color\e[0m"
+echo this text is with out an color
+echo -e "\e[33Hello this is yellow color\e[0m"
