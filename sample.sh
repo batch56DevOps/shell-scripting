9e85cccc1ce790afabb9a6797aafdf9698c2bcd2
Pavan <5058918-gitme3@users.noreply.gitlab.com>
2021-05-28 12:38:47 +0530

    new file

--- sample.sh
+++ sample.sh
@@ -8,7 +8,7 @@ cat /etc/passwd
 # printing
 
 echo -e "\nHola!!, \nthis is test case to use escape character"
-echo -e "\tthis helps to demonstrate and use
-          escape sequence. The rules and restrictions vary from state to state,
-          and it can get difficult to keep a track of it all.
-          That's why we've come up with a one-stop solution for your lockdown queries"
+echo -e "\t this helps to demonstrate and use
+            escape sequence. The rules and restrictions vary from state to state,
+            and it can get difficult to keep a track of it all.
+            That's why we've come up with a one-stop solution for your lockdown queries"
