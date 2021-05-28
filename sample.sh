ab87879bf5065239d82f62e289012c1cd8d298fc
Pavan <5058918-gitme3@users.noreply.gitlab.com>
2021-05-28 12:35:14 +0530

    new file

--- sample.sh
+++ sample.sh
@@ -4,4 +4,11 @@
 
 ls
 cat /etc/passwd
-ls -l /etc/sysconfig/network-scripts
+
+# printing
+
+echo -e "\nHola!!, \nthis is test case to use escape character"
+echo -e "\tthis helps to demonstrate and use
+          escape sequence. The rules and restrictions vary from state to state,
+          and it can get difficult to keep a track of it all.
+          That's why we've come up with a one-stop solution for your lockdown queries"
